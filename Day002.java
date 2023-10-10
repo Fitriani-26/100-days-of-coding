ba72a8189c2332e3085478ce68f82f5ffb7350c9
Fitriani <fitriani261102@gmail.com>
2023-10-10 21:43:25 +0800

    Day two

---
+++ Day002.java
@@ -0,0 +1,23 @@
+public class Day002 {
+    public static void main(String[] args) {
+        String nama = "Fitriani";
+        String prodi = "Informatika";
+        double tinggi = 1.2;
+        int umur = 21;
+        long tanggal_lahir = 26112002;
+        byte angkatan = 21;
+        short password = 13579;
+
+        System.out.println("Nama : "+ nama);
+        System.out.println("Prodi : "+ prodi);
+        System.out.println("Angkatan : "+angkatan);
+        System.out.println("Tanggal Lahir : " +tanggal_lahir);
+        System.out.println("Umur : "+umur+" Tahun");
+        System.out.println("TInggi : "+tinggi+" meter");
+        System.out.println("Password : " +password);
+
+
+        
+    }
+    
+}
