0f33910ff2a56264868dc1759e5dc383484e119e
Fitriani <fitriani261102@gmail.com>
2023-10-11 19:29:25 +0800

    Day Three

---
+++ Day003.java
@@ -0,0 +1,17 @@
+public class Day003 {
+    public static void main(String[] args) {
+        float bil1,bil2;
+        double bil3,bil4;
+        int bil5, bil6;
+        bil1 = 2.90f ;
+        bil2= 5.00f ;
+        bil3 = 3.500;
+        bil4 = 2.900;
+        bil5 = 4;
+        bil6 = 5;
+        System.out.println("bil1 + bil3 = "+ (bil1 + bil3));
+        System.out.println("bil2 - bil4 = "+ (bil2 - bil4));
+        System.out.println("bil5 * bil1 = "+(bil5 * bil1));
+        System.out.println("bil2 / bil6 = " + (bil2 / bil6));
+    }
+}
