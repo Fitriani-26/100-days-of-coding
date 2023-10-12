e22cfb4e5e06f29beac236250f6e26bc981314f8
Fitriani <fitriani261102@gmail.com>
2023-10-12 19:48:07 +0800

    Day Four

---
+++ Day004.java
@@ -0,0 +1,22 @@
+public class Day004 {
+    public static void main(String[] args) {
+        float bil1,bil2, bil7;
+        double bil3,bil4;
+        int bil5, bil6;
+        bil1 = 2.90f ;
+        bil2= 5.00f ;
+        bil3 = 3.500;
+        bil4 = 2.900;
+        bil5 = 4;
+        bil6 = 5;
+        bil7 = 3.50f;
+        System.out.println("Apakah bil1 > bil3 = "+ (bil1 > bil3));
+        System.out.println("Apakah bil2 < bil4 = "+ (bil2 < bil4));
+        System.out.println("apakah bil2 >= bil6 = "+(bil2 >= bil6));
+        System.out.println("Apakah bil2 <= bil5 = " + (bil2 <= bil5));
+        System.out.println("Apakah bil3 == bil7 = " +(bil3 == bil7));
+        System.out.println("Apakah bil6 != bil2 = " + (bil6 != bil2));
+
+    }
+    
+}
