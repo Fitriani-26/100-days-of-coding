332ce7f3fc30cac6f989125502bfa4fa1908aee0
Fitriani <fitriani261102@gmail.com>
2023-10-14 01:11:39 +0800

    Day Five

---
+++ Day005.java
@@ -0,0 +1,10 @@
+public class Day005 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("x dan y menghasilkan " + (x && y));
+        System.out.println("y or x menghasilkan " + (y || x));
+    }
+
+}
