e6600fb8a5e8295581c85386c0dcf2f7d93301df
Fitriani <fitriani261102@gmail.com>
2023-10-14 20:49:33 +0800

    Day six

---
+++ Day006.java
@@ -0,0 +1,14 @@
+public class Day006 {
+    public static void main(String[] args) {
+        int password = 261102;
+        String username = "Fitriani";
+
+        if (password ==  261102 && username == "Fitriani") {
+            System.out.println("Login Berhasil");
+        } else {
+            System.out.println("Login Gagal"); 
+        }
+    }
+
+    
+}
