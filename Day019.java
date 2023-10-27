2a0f3ae92ae48502aadc93e0136a9fbd79cd89a0
Fitriani <fitriani261102@gmail.com>
2023-10-27 21:53:00 +0800

    Casting

---
+++ Day019.java
@@ -0,0 +1,17 @@
+public class Day019 {
+    public static void main(String[] args) {
+        double angka_dob = 3.6 ;
+        int angka_int = (int) + angka_dob - 90;
+
+        long angka_long = 120;
+        byte angka_byte = (byte) + angka_long;
+
+        int angka_int1 = 90;
+        float angka_float = (float) + angka_int1 * 100;
+
+        System.out.println("Hasil Casting = " + angka_int);
+        System.out.println("Hasil Casting = " + angka_byte);
+        System.out.println("Hasil Casting = " + angka_float);
+    }
+    
+}
