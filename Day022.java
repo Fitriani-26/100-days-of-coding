b7874cf30cfff0dc7e08397e9b98066906960039
Fitriani <fitriani261102@gmail.com>
2023-10-30 20:28:20 +0800

    Array

---
+++ Day022.java
@@ -0,0 +1,13 @@
+public class Day022 {
+    public static void main(String[] args) {
+        String [] buah = {"Anggur", "Apel", "Rambutan", "Mangga", "Durian"};
+        Integer [] bil1 = {35, 40, 80, 55};
+
+        System.out.println(buah.length);
+        System.out.println(bil1.length);
+        System.out.println(buah[3]);
+        System.out.println(bil1[0]);
+
+    }
+    
+}
