cb47706d9d0c2638411be91e1a82f3473541a565
Fitriani <fitriani261102@gmail.com>
2023-10-30 20:29:48 +0800

    Min and Maks Value

---
+++ Day023.java
@@ -0,0 +1,11 @@
+public class Day023 {
+    public static void main(String[] args) {
+        System.out.println("Nilai Minimum Tipe data Integer = " +Integer.MIN_VALUE + " dan Maksimum = " +Integer.MAX_VALUE);
+        System.out.println("Nilai Minimum Tipe data Double = " +Double.MIN_VALUE + " dan Maksimum = " +Double.MAX_VALUE);
+        System.out.println("Nilai Minimum Tipe data Float = " +Float.MIN_VALUE + " dan Maksimum = " +Float.MAX_VALUE);
+        System.out.println("Nilai Minimum Tipe data Long = " +Long.MIN_VALUE + " dan Maksimum = " +Long.MAX_VALUE);
+        System.out.println("Nilai Minimum Tipe data Byte = " +Byte.MIN_VALUE + " dan Maksimum = " +Byte.MAX_VALUE);
+        System.out.println("Nilai Minimum Tipe data Short = " +Short.MIN_VALUE + " dan Maksimum = " +Short.MAX_VALUE);
+    }
+    
+}
