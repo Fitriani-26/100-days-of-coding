6506f8c7c27af2ffd9dff4ddf30b76bb0050b5a7
Fitriani <fitriani261102@gmail.com>
2023-11-04 22:45:08 +0800

    Classes

---
+++ Day027.java
@@ -0,0 +1,13 @@
+public class Day027 {
+    String merek = "Avanza";
+    String warna = "Hitam";
+
+
+    public static void main(String[] args) {
+        Day027 obj1 = new Day027();
+        Day027 obj2 = new Day027();
+        System.out.println(obj1.merek);
+        System.out.println(obj2.warna);
+    }
+    
+}
