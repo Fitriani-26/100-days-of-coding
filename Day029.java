ad4f7beac9332f439da8ec1afb13fd8147c8d627
Fitriani <fitriani261102@gmail.com>
2023-11-07 00:19:41 +0800

    Classes

---
+++ Day029.java
@@ -0,0 +1,15 @@
+public class Day029 {
+    char ukuran1 = 'M';
+    char ukuran2 = 'L';
+    char ukuran3 = 'S';
+    public static void main(String[] args) {
+        Day029 obj1 = new Day029();
+        Day029 obj2 = new Day029();
+        Day029 obj3 = new Day029();
+        System.out.println("Nama Fitriani, " + "Ukuran baju " +(obj1.ukuran3));
+        System.out.println("Nama Fani, " + "Ukuran baju " +(obj2.ukuran2));
+        System.out.println("Nama  Sofia, " + "Ukuran baju " +(obj3.ukuran1));
+        
+    }
+}
+    
