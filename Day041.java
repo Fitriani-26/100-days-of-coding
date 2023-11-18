64d4be75e6d9bc894555a5213603185f0deefa57
Fitriani <fitriani261102@gmail.com>
2023-11-18 23:17:48 +0800

    Increment and Decrement

---
+++ Day041.java
@@ -0,0 +1,15 @@
+public class Day041 {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 4;
+        //Decrement
+        System.out.println(--angka2);
+        System.out.println(angka2--);
+        System.out.println("Nilai Akhir " + angka2);
+        //Increment
+        System.out.println(++angka1);
+        System.out.println(angka1++);
+        System.out.println("Nilai Akhir " + angka1);
+    }
+    
+}
