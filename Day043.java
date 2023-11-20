704eea7f35c67ef4b2799cf91b4e569d4374a816
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-11-21 06:12:19 +0800

    For Each

---
+++ Day043.java
@@ -0,0 +1,8 @@
+public class Day043 {
+	public static void main(String[] args) {
+		Integer [ ] umur = {19,23,22,14,17,16,21,15,20,18};
+		for (Integer a : umur){
+			System.out.println("umur : " + a + " tahun");
+		}
+	}
+}
