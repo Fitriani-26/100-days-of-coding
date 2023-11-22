774acf8691edbe704f7955a670281384d5dd2a59
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-11-22 08:05:21 +0800

    return

---
+++ Day044.java
@@ -0,0 +1,9 @@
+public class Day044 {
+	static int luas(int panjang, int lebar){
+		return panjang * lebar;
+	}
+	public static void main(String[] args) { 
+	System.out.println("luas =  "+luas(10,2));
+		
+	}
+}
