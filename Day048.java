89ac9e567cf7bbff4ca08d4a86be8f31c9d4ea90
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-11-27 22:19:25 +0700

    Variabel

---
+++ Day048.java
@@ -0,0 +1,11 @@
+
+public class Day048 {
+    public static void main(String[] args) {
+        String nama = "Fitri";
+        nama = "Fitriani"; //Dapat  diubah
+        System.out.println(nama);
+
+        final int angka = 20;
+        System.out.println(angka);//tidak dapat di ubah 
+    }
+}
