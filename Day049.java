9f97f0f65b5a701efdb8f8bfb706fc7478a82419
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-11-27 22:20:38 +0700

    Method (String)

---
+++ Day049.java
@@ -0,0 +1,12 @@
+public class Day049 {
+    static void data (String nama, int umur){
+        System.out.println(nama + " umur " + umur + " Tahun");
+    }
+    public static void main(String[] args) {
+        data("Fitriani", 21);
+        data("Putri Sofia", 22);
+        data("Fani", 20);
+        
+    }
+    
+}
