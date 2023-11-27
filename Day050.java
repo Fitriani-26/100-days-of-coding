6a8cfb272804c93cf77ef60170ee8ec5bfbc8c54
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-11-27 22:21:30 +0700

    Method (int and double)

---
+++ Day050.java
@@ -0,0 +1,18 @@
+public class Day050 {
+    static int penjumlahan(int x, int y){
+        return x + y;
+        
+    }
+    static double perkalian(double a, double b){
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int mypenjumlahan = penjumlahan(3, 2);
+        double myperkalian = perkalian(10.0, 5.5);
+        System.out.println(mypenjumlahan);
+        System.out.println(myperkalian);
+        
+    }
+
+    
+}
