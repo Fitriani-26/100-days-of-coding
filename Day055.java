c43afc929ec9898f2136f8a5b7f7f4975dd710f5
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-12-04 06:34:31 +0700

    Ternary Operator (Boolean)

---
+++ Day055.java
@@ -0,0 +1,8 @@
+public class Day055 {
+    public static void main(String[] args) {
+        boolean kondisi = true;
+        String hasil = (kondisi) ? "True" : "False";
+       
+        System.out.println(hasil);
+    }  
+}
