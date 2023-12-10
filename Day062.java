4abb1c812db1440b23ce0b168d6283a13645f456
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-12-10 22:55:30 +0700

    Void Method

---
+++ Day062.java
@@ -0,0 +1,9 @@
+public class Day062 {
+        public static void printkata(String kata) { 
+        System.out.println(kata); 
+    } 
+    public static void main(String[] args) {
+        printkata("Hello, World!"); 
+    }
+}
+        
