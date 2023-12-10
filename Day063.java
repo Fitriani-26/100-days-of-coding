3ae66dcb1a801a4886303361307e3f4152b78a79
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2023-12-10 22:56:46 +0700

    Non void Method

---
+++ Day063.java
@@ -0,0 +1,12 @@
+public class Day063 {
+    public static int bil(int bil1, int bil2) 
+    { 
+        int jumlah = bil1 + bil2;
+        return jumlah; 
+    } 
+    public static void main(String[] args) {
+        int hasil = bil(2, 5);
+        System.out.println("Hasil = " + hasil);
+    }
+    
+}
