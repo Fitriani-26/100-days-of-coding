c27c9c1aa44e9b4c523036a77eb7b9aa457a2ef5
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2024-01-03 09:15:36 +0700

    StringBuffer(reverse)

---
+++ Day071.java
@@ -0,0 +1,15 @@
+public class Day071 {
+    public static void main(String[] args) {
+        StringBuffer sc = new StringBuffer("Selamat Menikmati");
+
+        // Menampilkan isi awal
+        System.out.println("Isi Awal: " + sc);
+
+        // Membalik urutan karakter menggunakan reverse
+        sc.reverse();
+
+        // Menampilkan hasil setelah pembalikan
+        System.out.println("Setelah Pembalikan: " + sc);
+    }
+    
+}
