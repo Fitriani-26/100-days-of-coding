4125a8c67a9bca9521f4243ffcadb7c8a33b1d70
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2024-01-17 22:22:03 +0700

    latihan

---
+++ Day090.java
@@ -0,0 +1,11 @@
+public class Day090 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 100; a++){
+            if (a % 4 == 0) {
+                System.out.println(a + " ");
+                
+            }
+        }
+    }
+    
+}
